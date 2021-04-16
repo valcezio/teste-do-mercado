9707e10bdc51ea4098c2df90cec4698bba95d3ce
valcezio <valcezio@users.noreply.github.com>
2021-04-15 23:39:46 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$conect = mysql_connect("localhost","root","qwe20147") or dir (mysql_error());
+$conect = mysql_connect("localhost","root","pass") or dir (mysql_error());
 $banco = mysql_select_db("negociacao") or die (mysql_error());
 ?>
